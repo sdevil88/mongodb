4935a332e75a7f3a24dc2ab78815e650d64de821
sgdevil88 <wwwghimiresagar88@gmail.com>
2023-12-01 17:21:35 +0545

    akip and limit  or unwind

---
+++ aggrigation.mongodb.js
@@ -0,0 +1,93 @@
+use("mernstack")
+
+// db.fruits.insertMany([
+//     {
+//       name: "Apple",
+  
+//       color: ["green", "red"],
+//       category: [
+//         {
+//           size: "small",
+//           price: 250,
+//         },
+//         {
+//           size: "medium",
+//           price: 350,
+//         },
+//       ],
+//     },
+  
+//     {
+//       name: "Guava",
+  
+//       color: ["red", "white"],
+//       category: [
+//         {
+//           size: "small",
+//           price: 75,
+//         },
+//         {
+//           size: "medium",
+//           price: 100,
+//         },
+//       ],
+//     },
+  
+//     {
+//       name: "mango",
+  
+//       color: ["yellow", "green"],
+//       category: [
+//         {
+//           size: "small",
+//           price: 60,
+//         },
+//         {
+//           size: "medium",
+//           price: 80,
+//         },
+//       ],
+//     },
+//   ]);
+
+
+
+
+//unwind method 1
+
+
+// db.fruits.aggregate([
+//   {$match: {
+//     name: "Apple"
+//   },
+
+// },{
+//   $unwind: "$color",
+// }
+// ])
+
+// undind method 2
+
+// db.fruits.aggregate([
+//   {$match: {
+//     name: "Apple"
+//   },
+
+// },{
+//   $unwind: {
+//     path: path,
+//     includeArrayIndex: 'string',
+//     preserveNullAndEmptyArrays: boolean
+//   }
+// }
+// ])
+
+
+
+
+
+
+
+
+
+
